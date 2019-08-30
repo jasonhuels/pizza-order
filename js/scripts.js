1c68e82a318660e0fd30ddd632205ceccc3f0915
Jason Huels <jasonhuels@yahoo.com>
2019-08-30 11:41:06 -0700

    add customer info gathering logic

--- js/scripts.js
+++ js/scripts.js
@@ -31,13 +31,34 @@ function Customer(firstName, lastName, orders) {
   this.firstName = firstName,
   this.lastName = lastName,
   this.orders = orders,
-  this.costOfOrders = 0;
+  this.costOfOrders = 0,
+  this.orderNumber = 0;
 }
 
+////////////////// Pizzaria Object ////////////////////////
+function Pizzaria() {
+  this.orderItems = [],
+  this.orderTotal = this.calculateTotal(),
+  this.orderNumber = 1,
+  this.orders = [""]; // fill 0 element with dummy so index matches orderNumber
+}
 
+Pizzaria.prototype.addCustomer = function(orderToAdd) {
+  this.orders.push(orderToAdd);
+}
+
+Pizzaria.prototype.calculateTotal = function() {
+  var output = 0;
+  for(let i=0; i<this.orderItems.length; i++) {
+    output += this.orderItems[i].price;
+  }
+  return output;
+}
 
 /////////////////// UI ////////////////////
 $(function() {
+  var pizzaria = new Pizzaria();
+
   $("#new-order").submit(function(event) {
     event.preventDefault();
     var newSize = $("#size").val();
@@ -45,6 +66,7 @@ $(function() {
     var toppingCheckbox = $("input:checkbox[name=topping]:checked");
     var newToppings = [];
     var pizza;
+    var orderTotal = 0;
 
     newToppings.push(cheeseRadio);
     toppingCheckbox.each(function(){
@@ -52,10 +74,25 @@ $(function() {
     });
 
     pizza = new Pizza(newSize, newToppings);
+    pizzaria.orderItems.push(pizza);
+    orderTotal = pizzaria.calculateTotal();
+    console.log(pizzaria.calculateTotal());
 
     $("#orders").show();
     $("#orders-list").append("<tr> <td>" + pizza.sizeAsString() + "</td> <td>" + newToppings + "</td><td>" + pizza.price + "</td></tr>");
 
+    $("#table-total").text('$' + orderTotal.toFixed().toString())
     $("#new-order").trigger("reset");
+  });
+
+  $("#customer-info").submit(function(event) {
+    event.preventDefault();
+    var firstName = $("#first-name").val() || "Loyal";
+    var lastName = $("#last-name").val() || "Customer";
+    var customer = new Customer(firstName, lastName, pizzaria.orderItems);
+
+    customer.orderTotal = pizzaria.calculateTotal();
+    customer.orderNumber = pizzaria.orderNumber;
+    pizzaria.orderNumber++;
   });
 });
